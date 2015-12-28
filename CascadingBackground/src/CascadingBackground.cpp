001df168e33c3e7bd528abb142b8866105e1ec2f
Anthony Tse <tsekwokyan@gmail.com>
2015-12-28 13:12:37 +0800

    Major refactor to classes

--- CascadingBackground/src/CascadingBackground.cpp
+++ CascadingBackground/src/CascadingBackground.cpp
@@ -3,119 +3,159 @@
 const int window_width = 240;
 const int window_height = 160;
 const float speed = 15.f;
-const int scale = 2;
+const int scale = 4;
+const sf::VideoMode vidMode{window_width * scale, window_height * scale};
+const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
+
+class App
+{
+public:
+    App();
+    void run();
+private:
+    void processEvents();
+    void update(sf::Time deltaTime);
+    void render();
+    void resetBackground();
+private:
+    sf::RenderWindow m_window;
+    sf::Texture m_bgTex; // Background back
+    sf::Texture m_fgTex; // Background front
+    sf::Texture m_dgTex; // Dialogue Box
+    sf::Sprite m_bg;
+    sf::Sprite m_fg;
+    sf::Sprite m_dg;
+    sf::Vector2i m_movement;
+};
+
+App::App()
+: m_window(vidMode, "Yay GBA!")
+, m_bgTex()
+, m_fgTex()
+, m_dgTex()
+, m_bg(m_bgTex)
+, m_fg(m_fgTex)
+, m_dg()
+, m_movement(1, -1)
+{
+    m_bgTex.loadFromFile("Media/Machinesbgpink.png"); //muahahah real men dont handle errorsssss
+	m_bgTex.setRepeated(true);
+	m_bgTex.setSmooth(true);
+
+	m_fgTex.loadFromFile("Media/Machinesfgpink.png");
+	m_fgTex.setRepeated(true);
+	//m_fgTex.setSmooth(true);
+
+	m_dgTex.loadFromFile("Media/DialogueBox.png");
+
+	m_bg.setTextureRect(sf::IntRect(0, 0, 368, 288)); // Get (64+240+64)x(64+160+64) of texture
+	m_bg.setOrigin(64, 64);
+	m_bg.scale(scale, scale);
+	//m_bg.setPosition(0, 0); //implied
+
+	m_fg.setTextureRect(sf::IntRect(0, 0, 368, 288));
+	m_fg.setOrigin(64, 64);
+	m_fg.scale(scale, scale);
+	//m_fg.setPosition(0, 0); //implied
+
+    m_dg.setTexture(m_dgTex);
+	m_dg.setPosition(32 * scale, 70 * scale) ;
+	m_dg.scale(scale, scale);
+}
+
+void App::run()
+{
+    // Main loop
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero; //lol temp solution for now
+	while (m_window.isOpen())
+	{
+        timeSinceLastUpdate += clock.restart();
+        while (timeSinceLastUpdate > TimePerFrame)
+        {
+            timeSinceLastUpdate -= TimePerFrame;
+            processEvents();
+            update(TimePerFrame);
+        }
+        render();
+	}
+}
+
+void App::processEvents()
+{
+    sf::Event event;
+    while (m_window.pollEvent(event))
+    {
+        // The 4 diagonal directions
+        if (event.type == sf::Event::KeyPressed)
+        {
+            if (event.key.code == sf::Keyboard::Numpad1)
+            {// foreground moves towards lower left
+                m_movement = sf::Vector2i(-1, 1);
+                resetBackground();
+            }
+            else if (event.key.code == sf::Keyboard::Numpad3)
+            {// foreground moves towards lower right
+                m_movement = sf::Vector2i(1, 1);
+                resetBackground();
+            }
+            else if (event.key.code == sf::Keyboard::Numpad7)
+            {// foreground moves towards upper left
+                m_movement = sf::Vector2i(-1, -1);
+                resetBackground();
+            }
+            else if (event.key.code == sf::Keyboard::Numpad9)
+            {// foreground moves towards upper right
+                m_movement = sf::Vector2i(1, -1);
+                resetBackground();
+            }
+        }
+
+        if (event.type == sf::Event::Closed)
+            m_window.close();
+    }
+}
+
+void App::update(sf::Time deltaTime)
+{
+    auto mov = sf::Vector2f(m_movement * scale) * speed * deltaTime.asSeconds();
+    m_fg.move(mov);
+    m_bg.move(-mov);
+    if (m_fg.getPosition().x * m_movement.x >= 64 * scale)
+        resetBackground();
+    /*
+    if (m_fg.getPosition().x * m_movement.x >= 64 * scale ||
+        m_fg.getPosition().y * m_movement.y >= 64 * scale)
+        m_fg.setPosition(0, 0);
+
+    m_bg.move(m_movement * -speed * scale * deltaTime.asSeconds());
+    if ( -m_bg.getPosition().x * m_movement.x >= 64 * scale ||
+         -m_bg.getPosition().y * m_movement.y >= 64 * scale)
+        m_bg.setPosition(0, 0);
+    */
+}
+
+void App::render()
+{
+    m_window.clear();
+
+    m_window.draw(m_bg);
+    m_window.draw(m_fg);
+    m_window.draw(m_dg);
+
+    m_window.display();
+}
+
+void App::resetBackground()
+{
+    m_fg.setPosition(0, 0);
+    m_bg.setPosition(0, 0);
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(window_width * scale,
-										  window_height * scale),
-							"Yay GBA!");
-	window.setFramerateLimit(60);
-
-	sf::Texture backgroundTex;
-	backgroundTex.loadFromFile("Media/Machinesbgpink.png");
-	backgroundTex.setRepeated(true);
-
-	sf::Texture foregroundTex;
-	foregroundTex.loadFromFile("Media/Machinesfgpink.png");
-	foregroundTex.setRepeated(true);
-
-	sf::Texture dialogueTex;
-	dialogueTex.loadFromFile("Media/DialogueBox.png");
-
-	sf::Sprite background(backgroundTex);
-	//background.setColor(COLOR_VARIABLE); can't figure out the color modulation!!
-	background.setTextureRect(sf::IntRect(0, 0, 368, 288)); // Get (64+240+64)x(64+160+64) of texture
-	background.setOrigin(64, 64);
-	background.scale(sf::Vector2f(scale, scale));
-	//background.setPosition(0, 0); //implied
-
-	sf::Sprite foreground(foregroundTex);
-	//foreground.setColor(COLOR_VARIABLE);
-	foreground.setTextureRect(sf::IntRect(0, 0, 368, 288));
-	foreground.setOrigin(64, 64);
-	foreground.scale(sf::Vector2f(scale, scale));
-	//foreground.setPosition(0, 0); //implied
-
-	sf::Sprite dialogue;
-	dialogue.setTexture(dialogueTex);
-	dialogue.setPosition(32 * scale, 70 * scale);
-	dialogue.scale(sf::Vector2f(scale, scale));
-
-	float x_direction = 1;
-	float y_direction = -1;
-
-	// Main loop
-	sf::Clock clock;
-	while (window.isOpen())
-	{
-		// Get deltaTime
-		sf::Time deltaTime = clock.restart();
-		// Process Events
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// The 4 diagonal directions
-			if (event.type == sf::Event::KeyPressed)
-			{
-				if (event.key.code == sf::Keyboard::Numpad1)
-				{// foreground moves towards lower left
-					x_direction = -1;
-					y_direction = 1;
-					background.setPosition(0, 0);
-					foreground.setPosition(0, 0);
-				}
-				else if (event.key.code == sf::Keyboard::Numpad3)
-				{// foreground moves towards lower right
-					x_direction = 1;
-					y_direction = 1;
-					background.setPosition(0, 0);
-					foreground.setPosition(0, 0);
-				}
-				else if (event.key.code == sf::Keyboard::Numpad7)
-				{// foreground moves towards upper left
-					x_direction = -1;
-					y_direction = -1;
-					background.setPosition(0, 0);
-					foreground.setPosition(0, 0);
-				}
-				else if (event.key.code == sf::Keyboard::Numpad9)
-				{// foreground moves towards upper right
-					x_direction = 1;
-					y_direction = -1;
-					background.setPosition(0, 0);
-					foreground.setPosition(0, 0);
-				}
-			}
-
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		// Update
-		foreground.move(sf::Vector2f(x_direction * speed * scale,
-									 y_direction * speed * scale)
-									 * deltaTime.asSeconds());
-		if (foreground.getPosition().x * x_direction >= 64 * scale ||
-			foreground.getPosition().y * y_direction >= 64 * scale)
-			foreground.setPosition(0, 0);
-
-		background.move(sf::Vector2f(x_direction * -speed * scale,
-									 y_direction * -speed * scale)
-									 * deltaTime.asSeconds());
-		if ( -background.getPosition().x * x_direction >= 64 * scale || // Yes, I know this might short circuit, but just
-			 -background.getPosition().y * y_direction >= 64 * scale)   // for the sake of clearness, I include both
-			background.setPosition(0, 0);
-
-		// Render
-		window.clear();
-
-		window.draw(background);
-		window.draw(foreground);
-		window.draw(dialogue);
-
-		window.display();
-	}
+    App app;
+    app.run();
 
 	return 0;
 }
